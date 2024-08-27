79b8c386ea317a25099738c18c4b2ddbd31241a5
RaphaelStinson <rafaelcustodio.com@gmail.com>
2024-08-27 10:55:28 -0300

    update

--- utilities/installer.py
+++
@@ -1,8 +0,0 @@
-# utilities/installer.py
-import os
-
-def install_dependencies():
-    os.system("pip install colored colorama pyfiglet watchdog")
-
-if __name__ == '__main__':
-    install_dependencies()
